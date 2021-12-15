b0ba393f5071179b9a4d816b9e9b29b850416f2c
burak eşit <esitburak@hotmail.com>
2021-12-15 22:49:03 +0300

    Add project files.

---
+++ 08052021-AdamAsmaca/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace _08052021_AdamAsmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSehir = new System.Windows.Forms.Label();
+            this.lbHarfler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblSehir
+            // 
+            this.lblSehir.AutoSize = true;
+            this.lblSehir.Location = new System.Drawing.Point(12, 9);
+            this.lblSehir.Name = "lblSehir";
+            this.lblSehir.Size = new System.Drawing.Size(0, 13);
+            this.lblSehir.TabIndex = 0;
+            // 
+            // lbHarfler
+            // 
+            this.lbHarfler.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lbHarfler.FormattingEnabled = true;
+            this.lbHarfler.Location = new System.Drawing.Point(420, 0);
+            this.lbHarfler.Name = "lbHarfler";
+            this.lbHarfler.Size = new System.Drawing.Size(380, 450);
+            this.lbHarfler.TabIndex = 3;
+            this.lbHarfler.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbHarfler);
+            this.Controls.Add(this.lblSehir);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSehir;
+        private System.Windows.Forms.ListBox lbHarfler;
+    }
+}
+
---
+++ 08052021-AdamAsmaca/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08052021_AdamAsmaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> cities;
+        string city;
+        List<char> guess;
+        int live = 0;
+        int lineCounter = 0;
+        Random random;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            live = 5;
+            lineCounter = 0;
+            lbHarfler.Items.Clear();
+
+            cities = new List<string>() { "İSTANBUL", "İZMİR", "ADANA", "LOS ANGELES" };
+            random = new Random();
+            city = cities[random.Next(cities.Count)];
+            
+            guess = new List<char>();
+
+            for (int i = 0; i < city.Length; i++)
+            {
+                if (city[i] != ' ')
+                {
+                    guess.Add('_');
+                    lineCounter++;
+                }
+                else
+                    guess.Add(' ');
+            }
+            
+            WriteWord();
+            this.Text = $"{live} hakkınız var";
+            MessageBox.Show("Lütfen bir tuşa basınız.");
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char letter = char.ToUpper(e.KeyChar);
+            MessageBox.Show(letter.ToString());
+
+            if (!char.IsLetter(letter))//harf değilse program sonuna git.
+            {
+                return;
+            }
+            if (lbHarfler.Items.Contains(letter) || char.IsWhiteSpace(letter))//listboxta daha önceden varsa veya boşluksa kapat.
+            {
+                return;
+            }
+            if (city.Contains(letter))//Kullanıcının girdiği harf şehir içerisinde var mı?
+            {
+                //Kullanıcı doğru bildi.
+                for (int i = 0; i < city.Length; i++)
+                {
+                    if (city[i] == letter)
+                    {
+                        guess[i] = letter;
+                        lineCounter--;
+                    }
+                }
+                WriteWord();
+                lbHarfler.Items.Add(letter);
+                if (lineCounter == 0)
+                {
+                    //MessageBox.Show("Kazandın");
+                    //EndGame("Tebrikler kazandınız\n\nTekrar oynamak ister misiniz?");
+                }
+                
+            }
+            else
+            {
+                //Kullanıcı yanlış harf girdi.
+
+                live--;
+                this.Text = $"{live} hakkınız var.";
+                if (live==0)
+                {
+                    //MessageBox.Show("Kaybettin");
+                    //EndGame("Tebrikler kazandınız\n\nTekrar oynamak ister misiniz?");
+                }
+            }
+        }
+
+        private void WriteWord()
+        {
+            lblSehir.Text = string.Empty;
+            foreach (char item in guess)
+            {
+                lblSehir.Text += item.ToString() + " ";
+            }
+        }
+
+        //void EndGame(string message)
+        //{
+        //    DialogResult dialogResult = new DialogResult();
+        //    dialogResult.
+        //}
+    }
+}
